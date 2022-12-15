934fcc4361ca2ad366008489315dca89fd1f5816
tuiu <13719283454@163.com>
2022-12-15 14:22:10 +0800

    chore: 编译之后的脚本

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import SteamUser from 'steam-user';
+import SteamTotp from 'steam-totp';
+
+const app = express();
+const client = new SteamUser;
+
+const SHARED_SECRET = "2DEBlBw2IKKIBJK4erlxXS5In5s=";
+
+app.get('/login', (req, res) => {
+
+  client.logOn({
+    // accountName: USERNAME,
+    // password: PASSWORD,
+    // rememberPassword: true,
+    loginKey: "TLsSmbVEczjV2CMbGtc"
+  });
+
+  client.on("loggedOn", () => {
+    res.json({
+      code: 200,
+      message: "登录成功" 
+    });
+  });
+
+  client.on("loginKey", (key) => {
+    console.log("loginKey: ", key);
+  });
+
+  client.on("error", (err) => {
+    res.json({
+      code: 0,
+      message: err.message
+    });
+    // console.log("err: ", err.name, err.message);
+  });
+
+  client.on("steamGuard", (domain, callback) => {
+
+    const code = SteamTotp.generateAuthCode(SHARED_SECRET);
+    callback(code);
+
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log("Listen Port 3000");
+});
