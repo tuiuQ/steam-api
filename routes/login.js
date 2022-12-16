95fa1380115c0544192ba4baf972d86e340986e9
tuiu <13719283454@163.com>
2022-12-16 14:20:11 +0800

    chore: 登录处理

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const SteamUser = require("steam-user");
+
+const user = new SteamUser;
 
 
-router.post("/", (_, res) => {
+router.get("/", (_, res) => {
+  res.send("post");
+});
+
+router.post("/", (req, res) => {
+
+  const param = req.body;
+
+  user.logOn({
+    accountName: param.username,
+    password: param.password,
+    twoFactorCode: param.authCode,
+    rememberPassword: param.rememberPassword,
+    loginKey: param.loginKey,
+    shared_secret: param.shared_secret
+  });
+
+  user.getAuthSecret((err, id, key) => {
+    console.log("err", err);
+    console.log("res: ", id, key);
+  });
+
+  user.on("loggedOn", (args) => {
+    console.log("login success", args);
+
+    user.on("loginKey", (key) => {
+      res.json({
+        code: 200,
+        data: {
+          loginKey: key
+        }
+      });
+    });
+
+  });
+
   res.json({
     code: 200
   })
+
 });
 
 
